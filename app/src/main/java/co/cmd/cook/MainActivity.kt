0d04cae896963e8062967c928daf7c38c11bd494
Juan Pablo <juanscigliano@gmail.com>
2021-08-21 11:35:57 +0200

    Add empty android project

---
+++ app/src/main/java/co/cmd/cook/MainActivity.kt
@@ -0,0 +1,18 @@
+package co.cmd.cook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.cmd.cook.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
