bad4947176934e70f182a0ba9ad19ac6276bdaeb
Juan Pablo <juanscigliano@gmail.com>
2021-08-22 13:04:53 +0200

    Add AuthRepository

---
+++ core/src/main/java/co/cmd/core/data/repository/AuthRepository.kt
@@ -0,0 +1,21 @@
+package co.cmd.core.data.repository
+
+import co.cmd.core.data.datasource.AuthDatasource
+import co.cmd.core.data.datasource.ConfigDatasource
+import co.cmd.core.domain.ClientID
+import co.cmd.core.domain.ClientSecretKey
+import co.cmd.core.domain.Token
+
+class AuthRepository(
+    private val networkDatasource: AuthDatasource,
+    private val configDatasource: ConfigDatasource
+
+) {
+
+    suspend fun fetchToken(clientID: ClientID, clientSecretKey: ClientSecretKey): Result<Token> =
+        networkDatasource.fetchToken(clientID, clientSecretKey)
+
+    fun getClientID(): ClientID = configDatasource.getClientID()
+
+    fun getClientSecretKet(): ClientSecretKey = configDatasource.getClientSecret()
+}
