3904a35eb7f29eed1ed9741413459d11ee69435b
Juan Pablo <juanscigliano@gmail.com>
2021-08-23 17:56:21 +0200

    Encode POST api call  with scope and grant_type while fetching token

--- app/src/main/java/co/cmd/cook/framework/api/AuthService.kt
+++ app/src/main/java/co/cmd/cook/framework/api/AuthService.kt
@@ -1,11 +1,18 @@
 package co.cmd.cook.framework.api
 
 import co.cmd.cook.framework.dto.TokenResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
 
+    @FormUrlEncoded
     @POST("/connect/token")
-    suspend fun fetchToken(@Header("Authorization") auth: String): TokenResponse
+    suspend fun fetchToken(
+        @Header("Authorization") auth: String,
+        @Field("scope") scope: String = "basic",
+        @Field("grant_type") grantType: String = "client_credentials",
+    ): TokenResponse
 }
