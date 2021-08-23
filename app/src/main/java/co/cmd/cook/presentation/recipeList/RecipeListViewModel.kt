9605fc3fb4e736a492be0f32985ebf3b2bc61705
Juan Pablo <juanscigliano@gmail.com>
2021-08-23 21:10:58 +0200

    Add ClickListener to ReciperAdapter. Add Navigation Channel in RecipeViewModel to handle Recipe clicl

--- app/src/main/java/co/cmd/cook/presentation/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/co/cmd/cook/presentation/recipeList/RecipeListViewModel.kt
@@ -1,24 +1,33 @@
 package co.cmd.cook.presentation.recipeList
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import co.cmd.cook.framework.datasource.paging.NETWORK_PAGE_SIZE
 import co.cmd.cook.framework.datasource.paging.PagingProductsDataSource
+import co.cmd.cook.util.NavigationCommand
 import co.cmd.core.domain.Recipe
 import co.cmd.core.interactors.GetRecipes
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class RecipeListViewModel(private val getRecipes: GetRecipes) : ViewModel() {
 
-    val recipeList: Flow<PagingData<Recipe>> = Pager(config = PagingConfig(pageSize = NETWORK_PAGE_SIZE, enablePlaceholders = false),
-        pagingSourceFactory = {
-            PagingProductsDataSource(getRecipes)
-        }).flow
+    private val _navigationActions = Channel<NavigationCommand>(capacity = CONFLATED)
 
+    // Exposed with receiveAsFlow to make sure that only one observer receives updates.
+    val navigationActions = _navigationActions.receiveAsFlow()
+
+    val recipeList: Flow<PagingData<Recipe>> =
+        Pager(config = PagingConfig(pageSize = NETWORK_PAGE_SIZE, enablePlaceholders = false),
+            pagingSourceFactory = {
+                PagingProductsDataSource(getRecipes)
+            }).flow
+
+    fun onRecipeItemClicked(recipe: Recipe) {
+        //  _navigationActions.trySend(NavigationCommand.To())
+    }
 }
