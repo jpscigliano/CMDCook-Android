ebe35ff9f8090422b933acc3aa5c8d668016b422
Juan Pablo <juanscigliano@gmail.com>
2021-08-23 20:58:03 +0200

    Add navigation command for wraping navigation events

---
+++ app/src/main/java/co/cmd/cook/util/NavigationCommand.kt
@@ -0,0 +1,10 @@
+package co.cmd.cook.util
+
+import androidx.navigation.NavDirections
+
+sealed class NavigationCommand {
+    data class To(val directions: NavDirections): NavigationCommand()
+    object Back: NavigationCommand()
+    data class BackTo(val destinationId: Int): NavigationCommand()
+    object ToRoot: NavigationCommand()
+}
