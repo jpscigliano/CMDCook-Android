671a92bea8a2a74eb2a4be679a38bc0879bfc8ea
Juan Pablo <juanscigliano@gmail.com>
2021-08-23 19:50:56 +0200

    Add RecipeListAdapter and layout for the row.

--- app/src/main/java/co/cmd/cook/presentation/recipeList/RecipeListFragment.kt
+++ app/src/main/java/co/cmd/cook/presentation/recipeList/RecipeListFragment.kt
@@ -2,19 +2,43 @@ package co.cmd.cook.presentation.recipeList
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import co.cmd.cook.databinding.FragmentRecipeListBinding
 import co.cmd.cook.presentation.BaseViewBindingFragment
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RecipeListFragment : BaseViewBindingFragment<FragmentRecipeListBinding>() {
-    companion object {
-
-        @JvmStatic
-        fun newInstance() = RecipeListFragment()
-    }
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentRecipeListBinding
         get() = FragmentRecipeListBinding::inflate
+    private val vm: RecipeListViewModel by viewModel()
+    private lateinit var adapter:RecipeListAdapter
 
     override fun onCreated() {
+        adapter = RecipeListAdapter()
+        setupUI()
+        setupObservers()
+    }
+
+    private fun setupUI() {
+        binding.recyclerRecipeList.adapter = adapter
+        binding.recyclerRecipeList.layoutManager = GridLayoutManager(requireContext(), 1)
+        binding.recyclerRecipeList.setHasFixedSize(true)
+    }
+
+    private fun setupObservers() {
+        lifecycleScope.launchWhenStarted {
+            vm.recipeList.collectLatest { pagingData ->
+                adapter.submitData(pagingData)
+            }
+        }
+
+
+        vm.liveRecipeList.observe(viewLifecycleOwner, { pagingData ->
+           // adapter.submitData(lifecycle,pagingData)
+        })
     }
 }
