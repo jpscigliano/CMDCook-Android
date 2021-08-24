a354b621baa149d4689cbdd4b93ed8ec5233b719
Juan Pablo <juanscigliano@gmail.com>
2021-08-24 11:53:49 +0200

    Update navigation version to 2.4. Plant timber

--- app/src/main/java/co/cmd/cook/CMDCookApp.kt
+++ app/src/main/java/co/cmd/cook/CMDCookApp.kt
@@ -4,11 +4,13 @@ import android.app.Application
 import co.cmd.cook.di.appComponent
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class CMDCookApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         startKoin {
             androidContext(this@CMDCookApp)
             modules(appComponent)
